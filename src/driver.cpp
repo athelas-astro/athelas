63b453ddc64b801ca636089818ded64d250d4d01
Brandon Barker <bbarker.py@protonmail.ch>
2024-07-07 23:37:53 -0400

    feat: average zone cycles and check if input deck exists, throw error if not

--- src/driver.cpp
+++ src/driver.cpp
@@ -6,6 +6,7 @@
  * Purpose : Main driver routine
  **/
 
+#include <filesystem> // std::exists
 #include <iostream>
 #include <string>
 #include <vector>
@@ -31,9 +32,15 @@
 int main( int argc, char *argv[] ) {
   // Check cmd line args
   if ( argc < 2 ) {
-    throw Error( "No input file passed! Do: ./main IN_FILE" );
+    throw Error( "! No input file passed! Do: ./main IN_FILE" );
   }
 
+  // ensure input deck exists
+  if ( !std::filesystem::exists( argv[1] ) ) {
+    throw Error( " ! Invalid path passed for athelas input deck!\n" );
+  }
+
+  // load input deck
   ProblemIn pin( argv[1] );
 
   /* --- Problem Parameters --- */
@@ -105,7 +112,8 @@ int main( int argc, char *argv[] ) {
     // --- Timer ---
     Kokkos::Timer timer_total;
     Kokkos::Timer timer_zone_cycles;
-    Real zc_ws = 0.0; // zone cycles / wall second
+    Real zc_ws      = 0.0; // zone cycles / wall second
+    Real time_cycle = 0.0;
 
     // --- Evolution loop ---
     int iStep   = 0;
@@ -115,7 +123,6 @@ int main( int argc, char *argv[] ) {
     std::cout << " ~ Step    t       dt       zone_cycles / wall_second\n"
               << std::endl;
     while ( t < t_end && iStep >= 0 ) {
-      timer_zone_cycles.reset( );
 
       // TODO: ComputeTimestep_Rad
       dt = ComputeTimestep_Fluid( state.Get_uCF( ), &Grid, &eos, CFL );
@@ -125,10 +132,6 @@ int main( int argc, char *argv[] ) {
 
       if ( t + dt > t_end ) {
         dt = t_end - t;
-      }
-
-      if ( iStep % i_print == 0 ) {
-        std::printf( " ~ %d %.5e %.5e %.5e \n", iStep, t, dt, zc_ws );
       }
 
       if ( !opts.do_rad ) {
@@ -148,6 +151,8 @@ int main( int argc, char *argv[] ) {
 #endif
 
       t += dt;
+      time_cycle += timer_zone_cycles.seconds( );
+      timer_zone_cycles.reset( );
 
       // Write state
       if ( iStep % i_write == 0 ) {
@@ -155,9 +160,12 @@ int main( int argc, char *argv[] ) {
         i_out += 1;
       }
 
+      if ( iStep % i_print == 0 ) {
+        zc_ws = static_cast<Real>( i_print ) * nX / time_cycle;
+        std::printf( " ~ %d %.5e %.5e %.5e \n", iStep, t, dt, zc_ws );
+        time_cycle = 0.0;
+      }
       iStep++;
-      Real time_cycle = timer_zone_cycles.seconds( );
-      zc_ws           = nX / time_cycle;
     }
 
     // --- Finalize timer ---
