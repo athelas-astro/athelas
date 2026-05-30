51342bae0969315b28383e4ceaf4daceade86c7b
Brandon Barker <bbarker.py@protonmail.ch>
2026-05-30 09:42:11 -0600

    chore: fmt

--- src/kokkos_abstraction.hpp
+++ src/kokkos_abstraction.hpp
@@ -766,7 +766,8 @@ struct array_type {
     }
   }
   KOKKOS_INLINE_FUNCTION // add operator
-      auto operator+=(const array_type &src) -> array_type & {
+      auto
+      operator+=(const array_type &src) -> array_type & {
     for (int i = 0; i < N; i++) {
       data[i] += src.data[i];
     }
